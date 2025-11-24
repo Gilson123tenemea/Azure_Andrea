eca1c2f60b11d8f6ceb31ed01744e591f4580002
Gilson123tenemea <tenemeaaguilar@gmail.com>
2025-11-24 11:05:44 -0500

    primer commit

---
+++ src/main/java/com/azure/welcome/AzureWelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.azure.welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureWelcomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/welcome/AzureWelcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.welcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWelcomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
